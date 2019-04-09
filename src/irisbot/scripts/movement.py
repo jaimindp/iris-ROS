8f54ebbe990072884ddafb8b933ffd9e38c104db
Lilly Clark <lilliamc@usc.edu>
2019-04-09 10:17:42 -0700

    semi-resolved m3pi name

--- src/irisbot/scripts/movement.py
+++ src/irisbot/scripts/movement.py
@@ -4,7 +4,9 @@ import rospy
 from std_msgs.msg import Bool, String
 from irisbot.msg import Pose, RotateCmd, DriveCmd
 import sys
-sys.path.append('/home/lilly/Desktop/DockerSpace')
+import os
+pathname = os.environ['HOME'] + '/Desktop/DockerSpace' # resolve this
+sys.path.append(pathname)
 
 try:
     import m3pi
